ca355bee1e17e325d6eb6ab166ff8066529ab986
telminov <telminov@soft-way.biz>
2015-06-15 11:22:49 +0300

    include templates and middleware in package

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ from distutils.core import setup
 
 setup(
     name='django-time-logger',
-    version='0.0.3',
+    version='0.0.4',
     description='Time logger for django',
     author='Telminov Sergey',
     url='https://github.com/telminov/django-time-logger',
-    packages=['time_logger',],
+    packages=[
+        'time_logger',
+        'time_logger/middleware',
+    ],
+    include_package_data=True,
     license='The MIT License',
 )
