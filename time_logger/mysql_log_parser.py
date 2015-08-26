955bffddf8519d368cb108c0e0d16fe364afa9a3
dyus <dyuuus@yandex.com>
2015-08-26 15:53:55 +0300

    #1 rewrite parser as generator

--- time_logger/mysql_log_parser.py
+++ time_logger/mysql_log_parser.py
@@ -4,14 +4,18 @@ import datetime
 import decimal
 
 _DATE_PAT = r"\d{6}\s+\d{1,2}:\d{2}:\d{2}"
-_SLOW_TIMESTAMP = re.compile(r"#\s+Time:\s+(" + _DATE_PAT + r")")
-_SLOW_USERHOST = re.compile(r"#\s+User@Host:\s+"
+
+_HEADER_VERSION = re.compile(r"(.+), Version: (\d+)\.(\d+)\.(\d+)(?:-(\S+))?")
+_HEADER_SERVER = re.compile(r"Tcp port:\s*(\d+)\s+Unix socket:\s+(.*)")
+
+_TIMESTAMP = re.compile(r"#\s+Time:\s+(" + _DATE_PAT + r")")
+_USERHOST = re.compile(r"#\s+User@Host:\s+"
                             r"(?:([\w\d]+))?\s*"
                             r"\[\s*([\w\d]+)\s*\]\s*"
                             r"@\s*"
                             r"([\w\d]*)\s*"
                             r"\[\s*([\d.]*)\s*\]")
-_SLOW_STATS = re.compile(r"#\sQuery_time:\s(\d*\.\d{1,6})\s*"
+_STATS = re.compile(r"#\sQuery_time:\s(\d*\.\d{1,6})\s*"
                          r"Lock_time:\s(\d*\.\d{1,6})\s*"
                          r"Rows_sent:\s(\d*)\s*"
                          r"Rows_examined:\s(\d*)")
@@ -23,19 +27,70 @@ class LogParserError(Exception):
 
 class MysqlSlowQueriesParser(object):
     def __init__(self, log_path):
-        self.log_path = log_path
+        self._stream = open(log_path, 'r')
+
+        line = self._get_next_line()
+        if line is not None and line.endswith('started with:'):
+            self._parse_headers()
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        entry = self._parse_entry()
+        if entry is None:
+            raise StopIteration
+        return entry
+
+    def _get_next_line(self):
+        line = self._stream.readline()
+        if not line:
+            return None
+        return line.rstrip('\r\n')
+
+    def _parse_headers(self):
+        # header strings:
+        #   1 run command and version
+        #   2 connection info
+        #   3 headers
+        # we do not need in all this stuff
+        self._get_next_line()
+        self._get_next_line()
+        line = self._get_next_line()
+        return line
+
+
+    def _parse_entry(self):
+        entry = {}
+        line = self._get_next_line()
+        if not line:
+            return None
+        if line.startswith('# Time:'):
+                timestamp = self._parse_time(line)
+                entry['time'] = timestamp
+                line = self._get_next_line()
+
+        if line.startswith('# User@Host:'):
+            private_user, unprivate_user, host, ip = self._parse_connection_info(line)
+            entry['user'] = private_user if private_user else unprivate_user
+            entry['host'] = host if host else ip
+            line = self._get_next_line()
+
+        if line.startswith('# Query_time:'):
+            query_time, lock_time, rows_sent, rows_examined = self._parse_statistics(line)
+            entry['query_time'] = decimal.Decimal(query_time)
+            entry['lock_time'] = decimal.Decimal(lock_time)
+            entry['rows_sent'] = int(rows_sent)
+            entry['rows_examined'] = int(rows_examined)
+            line = self._get_next_line()
+
+        queries = self._parse_queries(line)
+        entry['query_list'] = queries
+
+        return entry
 
     @staticmethod
     def _parse_line(regex, line):
-        """Parses a log line using given regular expression
-        regex[in]   a SRE_Match-object
-        line[in]    a string
-        This function takes a log line and matches the regular expresion given
-        with the regex argument. It returns the result of
-        re.MatchObject.groups(), which is a tuple.
-        Raises LogParserError on errors.
-        Returns a tuple.
-        """
         info = regex.match(line)
         if info is None:
             raise LogParserError('Failed parsing Slow Query line: %s' %
@@ -43,41 +98,29 @@ class MysqlSlowQueriesParser(object):
         return info.groups()
 
     def _parse_time(self, line):
-        info = self._parse_line(_SLOW_TIMESTAMP, line)
+        info = self._parse_line(_TIMESTAMP, line)
         return datetime.datetime.strptime(info[0], "%y%m%d %H:%M:%S")
 
     def _parse_connection_info(self, line):
         try:
-            info = self._parse_line(_SLOW_USERHOST, line)
+            info = self._parse_line(_USERHOST, line)
         except:
             info = ('', '', '', '0.0.0.0')
         return info
 
     def _parse_statistics(self, line):
-        return self._parse_line(_SLOW_STATS, line)
+        return self._parse_line(_STATS, line)
 
-    def parse(self):
-        with open(self.log_path) as f:
-            all_queries = []
-            query_info = {}
-            for line in f.readlines():
-                if line.startswith('# Time:'):
-                    timestamp = self._parse_time(line)
-                    query_info = {'time': timestamp}
-                    all_queries.append(query_info)
+    def _parse_queries(self, line):
+        query_string = []
+        while line:
+            if line.startswith('# Time:'):
+                break
+            query_string.append(line)
+            line = self._get_next_line()
+        return query_string
 
-                elif line.startswith('# User@Host:'):
-                    private_user, unprivate_user, host, ip = self._parse_connection_info(line)
-                    query_info['user'] = private_user if private_user else unprivate_user
-                    query_info['host'] = host if host else ip
-
-                elif line.startswith('# Query_time:'):
-                    query_time, lock_time, rows_sent, rows_examined = self._parse_statistics(line)
-                    query_info['query_time'] = decimal.Decimal(query_time)
-                    query_info['lock_time'] = decimal.Decimal(lock_time)
-                    query_info['rows_sent'] = int(rows_sent)
-                    query_info['rows_examined'] = int(rows_examined)
-                else:
-                    if 'time' in query_info:
-                        query_info.setdefault('query_string', []).append(line)
-            return all_queries
+# usage example
+# if __name__ == '__main__':
+#     for log in MysqlSlowQueriesParser('mysql-slow.log.1'):
+#         print log
